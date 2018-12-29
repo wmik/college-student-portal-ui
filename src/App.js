55780d334a1c2711b7cc327f9ea61d42ba43bbd0
wmik <williemik.wmik@gmail.com>
2018-12-29 17:38:49 +0300

    feat: breadcrumb navigation (now dynamic)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { Grid } from 'semantic-ui-react';
-import { Router } from '@reach/router';
+import { Router, Location } from '@reach/router';
 
 import FloatingSideMenu from './FloatingSideMenu';
 import ProfileForm from './ProfileForm';
@@ -18,7 +18,20 @@ function App() {
       <Grid padded stackable>
         <Grid.Row>
           <Grid.Column style={{ marginLeft: '3rem' }}>
-            <BreadcrumbNav />
+            <Location>
+              {({ location }) => {
+                function capitalize(word) {
+                  return word[0].toUpperCase() + word.slice(1);
+                }
+                if (location.pathname !== '/') {
+                  const [section, page] = location.pathname
+                    .slice(1)
+                    .split('/')
+                    .map(capitalize);
+                  return <BreadcrumbNav section={section} page={page} />;
+                }
+              }}
+            </Location>
           </Grid.Column>
         </Grid.Row>
         <Grid.Column width={4}>
--- src/BreadcrumbNav.js
+++ src/BreadcrumbNav.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Breadcrumb } from 'semantic-ui-react';
 
-function BreadCrumbNav() {
+function BreadCrumbNav({ section, page }) {
   return (
     <Breadcrumb size="big">
-      <Breadcrumb.Section>Settings</Breadcrumb.Section>
+      <Breadcrumb.Section>{section}</Breadcrumb.Section>
       <Breadcrumb.Divider icon="right chevron" />
-      <Breadcrumb.Section link>Profile</Breadcrumb.Section>
+      <Breadcrumb.Section link>{page}</Breadcrumb.Section>
     </Breadcrumb>
   );
 }
